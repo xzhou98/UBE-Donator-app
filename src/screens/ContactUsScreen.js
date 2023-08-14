ba9a4dc34bfe5c0b239e2408c584ebca99a71261
Xiangyu Zhou <357038667@qq.com>
2023-08-14 16:29:50 -0400

    update contact us screen

--- src/screens/ContactUsScreen.js
+++ src/screens/ContactUsScreen.js
@@ -26,29 +26,41 @@ const ContactUsScreen = () => {
 
   return (
     <ScrollView>
-      <View style={{alignItems: 'center', margin: 15}}>
+      {/* <View style={{alignItems: 'center', margin: 15}}>
         <Text style={[styles.title]}>Resource List</Text>
-      </View>
+      </View> */}
 
-      <View style={{marginHorizontal: 15}}>
-        <Text style={[styles.basetext]}>Question about the Study:</Text>
+      <View style={{marginHorizontal: '5%'}}>
+        <Text style={[styles.basetext]}>
+          For Questions or Thoughts Regarding The Study Contact:
+        </Text>
+
+        <Text style={[styles.boldText]}>{`Dr. Douglas Zytko`}</Text>
         <Text style={[styles.text]}>
-          {`Dr. Douglas Zytko \nAssistant Professor \nDepartment of Computer Science and Engineering\nzytko@oakland.edu \n(609)-313-8009`}
+          {`Assistant Professor \nDepartment of Computer Science and Engineering\nzytko@oakland.edu \n(609)-313-8009`}
+        </Text>
+
+        <Text style={[styles.boldText]}>{`Dr. Kelly Berishaj`}</Text>
+        <Text style={[styles.text]}>
+          {`Special Instructor \nSchool of Nursing\nberishaj@oakland.edu \n(248) 364-8750`}
         </Text>
       </View>
 
-      <View style={{marginHorizontal: 15}}>
+      <View style={{marginHorizontal: '5%'}}>
         <Text style={[styles.basetext]}>Need Help?</Text>
-        <Text style={{color: 'black', fontSize: 15}}>
-          {`If the data you provided today brought up thoughts about past or current experiences that you want to talk more about or explore, below are some organizations you can contact:`}
+        <Text style={[styles.basetext]}>Emergency</Text>
+        <Text style={{color: 'black', fontSize: 15, marginVertical: 10}}>
+          {`If you are suffering from a life-threatening injury or illness that requires emergent medical assistance OR if you are in a dangerous or unsafe situation that requires immediate police response:`}
         </Text>
+        <Text style={[styles.text]}>DIAL 911</Text>
+        <Text style={[styles.basetext]}>Other</Text>
 
         {/* 1 */}
         <TouchableOpacity
           style={{
             borderBottomWidth: 2,
-            marginHorizontal: '10%',
-            marginTop: 20,
+            // marginHorizontal: '5%',
+            marginTop: 15,
             flexDirection: 'row',
           }}
           onPress={() => {
@@ -57,9 +69,7 @@ const ContactUsScreen = () => {
             setCategories(temp);
             setRefresh(!refresh);
           }}>
-          <Text style={{flex: 6, color: 'black', fontSize: 15}}>
-            HAVEN of Oakland County
-          </Text>
+          <Text style={[styles.basetext]}>Sexual Assault:</Text>
           <MaterialIcons
             style={{color: 'black', flex: 1}}
             name={categories[0] ? 'add' : 'remove'}
@@ -69,12 +79,19 @@ const ContactUsScreen = () => {
         <View>
           {categories[0] ? (
             <View>
+              <Text
+                style={{
+                  color: 'black',
+                  fontSize: 15,
+                  marginLeft: '10%',
+                  marginTop: 10,
+                }}>
+                {`Rape, Abuse, and Incest National Network (RAINN)`}
+              </Text>
               <Text style={[styles.italic]}>
-                {`Specializing in supportive services for people who have experienced sexual assault or abuse in relationships`}
+                {`The National Sexual Assault Hotline gives you access to a range of free services offered by sexual assault service providers`}
               </Text>
-              <Text style={[styles.text]}>
-                {`Crisis & Support Line 24/7\n248-334-1274 \nwww.haven-oakland.org `}
-              </Text>
+              <Text style={[styles.text]}>{`1-800.656.HOPE (4673)`}</Text>
             </View>
           ) : (
             <></>
@@ -85,8 +102,8 @@ const ContactUsScreen = () => {
         <TouchableOpacity
           style={{
             borderBottomWidth: 2,
-            marginHorizontal: '10%',
-            marginTop: 20,
+            // marginHorizontal: '5%',
+            marginTop: 15,
             flexDirection: 'row',
           }}
           onPress={() => {
@@ -95,9 +112,7 @@ const ContactUsScreen = () => {
             setCategories(temp);
             setRefresh(!refresh);
           }}>
-          <Text style={{flex: 6, color: 'black', fontSize: 15}}>
-            Common Ground 24/7
-          </Text>
+          <Text style={[styles.basetext]}>Mental Health:</Text>
           <MaterialIcons
             style={{color: 'black', flex: 1}}
             name={categories[1] ? 'add' : 'remove'}
@@ -107,294 +122,80 @@ const ContactUsScreen = () => {
         <View>
           {categories[1] ? (
             <View>
-              <Text style={[styles.italic]}>
-                {`Offers crisis intervention and mental health services for individuals in crisis or those who have experienced trauma.`}
+              {/* 1 */}
+              <Text style={{color: 'black', fontSize: 15}}>
+                If the data you provided today brought up thoughts about past or
+                current experiences that you want to talk more about or explore:
               </Text>
-              <Text style={[styles.text]}>
-                {`1-800-273-TALK (8255)\nCan text to 988\nSpanish\nEspañol: 1-888-628-9454`}
+              <Text
+                style={{
+                  color: 'black',
+                  fontSize: 15,
+                  marginLeft: '10%',
+                  marginTop: 10,
+                }}>
+                {`National Institute of Mental Health`}
               </Text>
-            </View>
-          ) : (
-            <></>
-          )}
-        </View>
-
-        {/* 3 */}
-        <TouchableOpacity
-          style={{
-            borderBottomWidth: 2,
-            marginHorizontal: '10%',
-            marginTop: 20,
-            flexDirection: 'row',
-          }}
-          onPress={() => {
-            let temp = categories;
-            temp[2] = !temp[2];
-            setCategories(temp);
-            setRefresh(!refresh);
-          }}>
-          <Text style={{flex: 6, color: 'black', fontSize: 15}}>
-          National Suicide Prevention Lifeline 24/7
-          </Text>
-          <MaterialIcons
-            style={{color: 'black', flex: 1}}
-            name={categories[2] ? 'add' : 'remove'}
-            size={20}
-          />
-        </TouchableOpacity>
-        <View>
-          {categories[2] ? (
-            <View>
-              <Text style={[styles.italic]}>
-                {`Provides free and confidential support to individuals in suicidal crisis or emotional distress.`}
-              </Text>
-              <Text style={[styles.text]}>
-                {`1-800-273-TALK (8255)\nCan text to 988\nSpanish\nEspañol: 1-888-628-9454`}
-              </Text>
-            </View>
-          ) : (
-            <></>
-          )}
-        </View>
-
-        {/* 4 */}
-        <TouchableOpacity
-          style={{
-            borderBottomWidth: 2,
-            marginHorizontal: '10%',
-            marginTop: 20,
-            flexDirection: 'row',
-          }}
-          onPress={() => {
-            let temp = categories;
-            temp[3] = !temp[3];
-            setCategories(temp);
-            setRefresh(!refresh);
-          }}>
-          <Text style={{flex: 6, color: 'black', fontSize: 15}}>
-          Crisis Text Line 24/7
-          </Text>
-          <MaterialIcons
-            style={{color: 'black', flex: 1}}
-            name={categories[3] ? 'add' : 'remove'}
-            size={20}
-          />
-        </TouchableOpacity>
-        <View>
-          {categories[3] ? (
-            <View>
-              <Text style={[styles.italic]}>
-                {`Offers free 24/7 text-based support for people in crisis, including those experiencing suicidal thoughts.`}
-              </Text>
-              <Text style={[styles.text]}>
-                {`Text HOME to 741-741`}
-              </Text>
-            </View>
-          ) : (
-            <></>
-          )}
-        </View>
-
-
-        {/* 5 */}
-        <TouchableOpacity
-          style={{
-            borderBottomWidth: 2,
-            marginHorizontal: '10%',
-            marginTop: 20,
-            flexDirection: 'row',
-          }}
-          onPress={() => {
-            let temp = categories;
-            temp[4] = !temp[4];
-            setCategories(temp);
-            setRefresh(!refresh);
-          }}>
-          <Text style={{flex: 6, color: 'black', fontSize: 15}}>
-          Suicide Prevention Resource Center
-          </Text>
-          <MaterialIcons
-            style={{color: 'black', flex: 1}}
-            name={categories[4] ? 'add' : 'remove'}
-            size={20}
-          />
-        </TouchableOpacity>
-        <View>
-          {categories[4] ? (
-            <View>
-              <Text style={[styles.italic]}>
-                {`Provides resources and support for suicide prevention, including training programs and research.`}
-              </Text>
-              <Text style={[styles.text]}>
-                {`www.sprc.org`}
-              </Text>
-            </View>
-          ) : (
-            <></>
-          )}
-        </View>
-
-        {/* 6 */}
-        <TouchableOpacity
-          style={{
-            borderBottomWidth: 2,
-            marginHorizontal: '10%',
-            marginTop: 20,
-            flexDirection: 'row',
-          }}
-          onPress={() => {
-            let temp = categories;
-            temp[5] = !temp[5];
-            setCategories(temp);
-            setRefresh(!refresh);
-          }}>
-          <Text style={{flex: 6, color: 'black', fontSize: 15}}>
-          National Institute of Mental Health
-          </Text>
-          <MaterialIcons
-            style={{color: 'black', flex: 1}}
-            name={categories[5] ? 'add' : 'remove'}
-            size={20}
-          />
-        </TouchableOpacity>
-        <View>
-          {categories[5] ? (
-            <View>
               <Text style={[styles.italic]}>
                 {`Conducts research on mental health and provides information and resources for the public and healthcare professionals.`}
               </Text>
-              <Text style={[styles.text]}>
-                {`www.nimh.nih.gov`}
+              <Text style={[styles.text]}>{`www.nimh.nih.gov`}</Text>
+
+              {/* 2 */}
+              <Text
+                style={{
+                  color: 'black',
+                  fontSize: 15,
+                  marginLeft: '10%',
+                  marginTop: 10,
+                }}>
+                {`National Suicide Prevention Lifeline 24/7`}
               </Text>
-            </View>
-          ) : (
-            <></>
-          )}
-        </View>
-
-
-        {/* 7 */}
-        <TouchableOpacity
-          style={{
-            borderBottomWidth: 2,
-            marginHorizontal: '10%',
-            marginTop: 20,
-            flexDirection: 'row',
-          }}
-          onPress={() => {
-            let temp = categories;
-            temp[6] = !temp[6];
-            setCategories(temp);
-            setRefresh(!refresh);
-          }}>
-          <Text style={{flex: 6, color: 'black', fontSize: 15}}>
-          Pawley Hall Counseling Center
-          </Text>
-          <MaterialIcons
-            style={{color: 'black', flex: 1}}
-            name={categories[6] ? 'add' : 'remove'}
-            size={20}
-          />
-        </TouchableOpacity>
-        <View>
-          {categories[6] ? (
-            <View>
               <Text style={[styles.italic]}>
-                {`Offers counseling and mental health services to students and faculties at Oakland University.`}
+                {`Provides free and confidential support to individuals in suicidal crisis or emotional distress`}
               </Text>
-              <Text style={[styles.text]}>
-                {`Oakland University\nPawley Hall\n(248) 370-2633`}
+              <Text
+                style={{
+                  color: 'black',
+                  fontSize: 15,
+                  marginLeft: '10%',
+                }}>{`1-800-273-TALK (8255)`}</Text>
+              <Text
+                style={{
+                  color: 'black',
+                  fontSize: 15,
+                  marginLeft: '10%',
+                }}>{`Can text to 988`}</Text>
+              <Text
+                style={{
+                  color: 'black',
+                  fontSize: 15,
+                  marginLeft: '10%',
+                }}>{`Spanish`}</Text>
+              <Text style={[styles.text]}>{`Español: 1-888-628-9454h`}</Text>
+
+              {/* 3 */}
+              <Text
+                style={{
+                  color: 'black',
+                  fontSize: 15,
+                  marginLeft: '10%',
+                  marginTop: 10,
+                }}>
+                {`Crisis Text Line 24/7`}
               </Text>
-            </View>
-          ) : (
-            <></>
-          )}
-        </View>
-
-
-        {/* 8 */}
-        <TouchableOpacity
-          style={{
-            borderBottomWidth: 2,
-            marginHorizontal: '10%',
-            marginTop: 20,
-            flexDirection: 'row',
-          }}
-          onPress={() => {
-            let temp = categories;
-            temp[7] = !temp[7];
-            setCategories(temp);
-            setRefresh(!refresh);
-          }}>
-          <Text style={{flex: 6, color: 'black', fontSize: 15}}>
-          Oakland Family Services
-          </Text>
-          <MaterialIcons
-            style={{color: 'black', flex: 1}}
-            name={categories[7] ? 'add' : 'remove'}
-            size={20}
-          />
-        </TouchableOpacity>
-        <View>
-          {categories[7] ? (
-            <View>
               <Text style={[styles.italic]}>
-                {`Provides counseling, education, and support services for individuals and families in the community`}
+                {`Offers free 24/7 text-based support for people in crisis, including those experiencing suicidal thoughts.`}
               </Text>
-              <Text style={[styles.text]}>
-                {`Rochester Hills\n(866) 903-8955`}
-              </Text>
+              <Text style={[styles.text]}>{`Text HOME to 741-741`}</Text>
+
+
             </View>
           ) : (
             <></>
           )}
         </View>
-
-
-        {/* 9 */}
-        <TouchableOpacity
-          style={{
-            borderBottomWidth: 2,
-            marginHorizontal: '10%',
-            marginTop: 20,
-            flexDirection: 'row',
-          }}
-          onPress={() => {
-            let temp = categories;
-            temp[8] = !temp[8];
-            setCategories(temp);
-            setRefresh(!refresh);
-          }}>
-          <Text style={{flex: 6, color: 'black', fontSize: 15}}>
-          Community Health and Social services
-          </Text>
-          <MaterialIcons
-            style={{color: 'black', flex: 1}}
-            name={categories[8] ? 'add' : 'remove'}
-            size={20}
-          />
-        </TouchableOpacity>
-        <View>
-          {categories[8] ? (
-            <View>
-              <Text style={[styles.italic]}>
-                {`Offers a range of health and social services, including mental health support and counseling, to underserved populations in the community.`}
-              </Text>
-              <Text style={[styles.text]}>
-                {`5635 West Fort St., Detroit, MI 48209\n(313) 849-3920`}
-              </Text>
-            </View>
-          ) : (
-            <></>
-          )}
-        </View>
-
-        <Text style={{color: 'black', fontSize: 15, marginVertical:20}}>
-          {`If you are suffering from a life-threatening injury or illness that requires emergent medical assistance OR if you are in a dangerous or unsafe situation that requires immediate police response:`}
-        </Text>
-
-        <Text style={{color: 'black', fontSize: 15, fontWeight:'bold', marginLeft:'10%', marginBottom:50}}>911</Text>
-
+        <View style={{margin:30}}/>
       </View>
     </ScrollView>
   );
@@ -409,6 +210,7 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
   },
   basetext: {
+    flex: 5,
     // backgroundColor: '#f2f5f8',
     color: '#161924',
     fontSize: 16,
@@ -417,16 +219,22 @@ const styles = StyleSheet.create({
   text: {
     color: 'black',
     fontSize: 15,
-    marginVertical: 15,
-    marginHorizontal: '10%',
+    marginBottom: 20,
+    marginLeft: '10%',
     // fontWeight: 'bold',
+  },
+  boldText: {
+    color: 'black',
+    fontSize: 15,
+    marginTop: 15,
+    marginHorizontal: '10%',
+    fontWeight: 'bold',
   },
   italic: {
     color: '#71a0ed',
-    marginHorizontal: '10%',
+    marginLeft: '10%',
     fontStyle: 'italic',
     fontSize: 15,
-    marginBottom: -15,
   },
 });
 
